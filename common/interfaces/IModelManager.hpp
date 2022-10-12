11baf1f8f34553f03c514e7c6e787cf1da8eab04
Admer <49600504+Admer456@users.noreply.github.com>
2022-10-12 23:19:19 +0200

    IModelManager: access models via index instead of obtaining a list of them

--- common/interfaces/IModelManager.hpp
+++ common/interfaces/IModelManager.hpp
@@ -18,5 +18,7 @@ public:
 	virtual Render::IModel* 	CreateModel( const Render::ModelDesc& desc ) = 0;
 	virtual bool				UpdateModel( Render::IModel* model, const Render::ModelDesc& desc ) = 0;
 	virtual void				DestroyModel( Render::IModel* model ) = 0;
-	virtual const Vector<Render::IModel*>& GetModels() const = 0;
+
+	virtual size_t				GetNumModels() const = 0;
+	virtual Render::IModel*		GetModel( uint32_t index ) const = 0;
 };
